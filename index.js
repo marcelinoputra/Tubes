0dd249a9bdec8b0806138c12bc8e7a2e1fda3bb4
marcelinoputra <marcelinoputra0701@gmail.com>
2023-06-05 20:44:05 +0700

    udh bisa login

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function hashPassword(password) {
 const pool = mysql.createPool({
     user: 'root',
     password: '',
-    database: 'ide',
+    database: 'tubes',
     host: 'localhost'
 });
 
@@ -48,7 +48,36 @@ const dbConnect = () => {
     })
 }
 
-app.get('/', async(req, res) =>{
+const authMember = async (req, res, next) => {
+    if (req.session.username) {
+        const conn = await dbConnect();
+        const query = `
+            SELECT *
+            FROM pengguna
+            WHERE jabatan = "Member"
+        `;
+        conn.query(query, (err, result) => {
+            if (err) {
+                console.error(err);
+                res.sendStatus(500);
+            } else {
+                if (result.length > 0) {
+                    console.log(result);
+                    // Jika pengguna sudah login dan memiliki role admin, lanjutkan ke halaman yang diminta
+                    next();
+                } else {
+                    // Jika pengguna tidak memiliki role admin, tampilkan halaman forbidden
+                    res.status(403).send('forbidden');
+                }
+            }
+        });
+    } else {
+        // Jika pengguna belum login, redirect ke halaman login
+        res.redirect('/login');
+    }
+};
+
+app.get('/', authMember, async(req, res) =>{
     const conn = await dbConnect();
     res.render('mainUser');
 });
@@ -76,6 +105,44 @@ app.get('/loginAdmin', async(req, res) =>{
 app.get('/loginLeader', async(req, res) =>{
     const conn = await dbConnect();
     res.render('loginLeader');
+});
+
+app.post('/login', async (req, res) => {
+    try {
+        const conn = await dbConnect();
+        const username = req.body.username;
+        const password = req.body.password;
+        const hashedPassword = hashPassword(password);
+        //cek apakah username dan password kosong
+        const query = `SELECT * FROM pengguna WHERE username = ? AND password = ?`;
+        if (username && password) {
+            conn.query(query, [username, hashedPassword], (err, results) => {
+                if (err) {
+                    console.error(err);
+                    res.sendStatus(500);
+                } else {
+                    if (results.length > 0) {
+                        console.log('berhasil');
+                        // Tambahkan session dengan nama pengguna
+                        req.session.username = results[0].username;
+                        req.session.name = results[0].name;
+                        req.session.userID = results[0].userID;
+                        // Redirect ke halaman utama (tabel users)
+                        res.redirect('/');
+                    } else {
+                        // Jika akun tidak ditemukan, tetap berada di halaman login
+                        res.redirect('/login');
+                    }
+                }
+            });
+        } else {
+            // Jika username atau password kosong
+            console.log('login gagal, username dan password harus diisi');
+        }
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
 });
 
 
