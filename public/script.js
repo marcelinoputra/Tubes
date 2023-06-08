a7ce360e78a42904e48545c8dfd2bec27a0be76b
VincentMarkk <vincent.markwijaya@gmail.com>
2023-06-08 19:43:19 +0700

    css main

--- public/script.js
+++ public/script.js
@@ -1,71 +1,78 @@
 const searchInput = document.getElementById("searchInput");
 const searchResults = document.getElementById("searchResults");
-
+const searchButton = document.getElementById("searchButton");
+searchButton.addEventListener("click", () => {
+  document.getElementById("searchPopup").style.display = "grid";
+});
+document.getElementById("searchCloseButton").addEventListener("click", () => {
+  document.getElementById("searchPopup").style.display = "none";
+});
 searchInput.addEventListener("input", function () {
-    const searchQuery = searchInput.value.trim().toLowerCase();
+  const searchQuery = searchInput.value.trim().toLowerCase();
 
-    // Clear previous search results
-    searchResults.innerHTML = "";
+  // Clear previous search results
+  searchResults.innerHTML = "";
 
-    // Perform search and display results
-    if (searchQuery !== "") {
-        const searchData = performSearch(searchQuery);
-        displaySearchResults(searchData);
-    }
+  // Perform search and display results
+  if (searchQuery !== "") {
+    const searchData = performSearch(searchQuery);
+    displaySearchResults(searchData);
+  }
 });
 
 // Function to perform the search (dummy implementation)
 function performSearch(query) {
-    // Replace this with your actual search logic
-    const data = ["Result 1", "Result 2", "Result 3"];
+  // Replace this with your actual search logic
+  const data = ["Result 1", "Result 2", "Result 3"];
 
-    // Filter the data based on the search query
-    const filteredData = data.filter((item) =>
-        item.toLowerCase().includes(query)
-    );
+  // Filter the data based on the search query
+  const filteredData = data.filter((item) =>
+    item.toLowerCase().includes(query)
+  );
 
-    return filteredData;
+  return filteredData;
 }
 
 // Function to display the search results
 function displaySearchResults(results) {
-    if (results.length === 0) {
-        const noResultsItem = document.createElement("li");
-        noResultsItem.textContent = "No results found.";
-        searchResults.appendChild(noResultsItem);
-    } else {
-        results.forEach((result) => {
-            const listItem = document.createElement("li");
-            listItem.textContent = result;
-            searchResults.appendChild(listItem);
-        });
-    }
+  if (results.length === 0) {
+    const noResultsItem = document.createElement("li");
+    noResultsItem.textContent = "No results found.";
+    searchResults.appendChild(noResultsItem);
+  } else {
+    results.forEach((result) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = result;
+      searchResults.appendChild(listItem);
+    });
+  }
 }
 
-document.getElementById("premium-button").addEventListener("click", function () {
+document
+  .getElementById("premium-button")
+  .addEventListener("click", function () {
     // Function buat popup premium
     // function showPremiumPopup() {
-    document.getElementById("premiumPopup").style.display="flex";
-})
+    document.getElementById("premiumPopup").style.display = "flex";
+  });
 document.getElementById("cancelButton").addEventListener("click", function () {
-    // Function buat popup premium
-    // function showPremiumPopup() {
-    document.getElementById("premiumPopup").style.display="none";
-})
-
-
-var musicPlayer = document.getElementById('musicPlayer');
-var playButton = document.getElementById('playButton');
-var pauseButton = document.getElementById('pauseButton');
-
-playButton.addEventListener('click', function () {
-    musicPlayer.play();
+  // Function buat popup premium
+  // function showPremiumPopup() {
+  document.getElementById("premiumPopup").style.display = "none";
 });
 
-pauseButton.addEventListener('click', function () {
-    musicPlayer.pause();
+var musicPlayer = document.getElementById("musicPlayer");
+var playButton = document.getElementById("playButton");
+var pauseButton = document.getElementById("pauseButton");
+
+playButton.addEventListener("click", function () {
+  musicPlayer.play();
+});
+
+pauseButton.addEventListener("click", function () {
+  musicPlayer.pause();
 });
 
 document.getElementById("profile-btn").addEventListener("click", function () {
-    document.getElementById("logOutPopup").classList.toggle('hidden');
-})
+  document.getElementById("logOutPopup").classList.toggle("hidden");
+});
