1c5f4d4c5fd5b403ce19f0614b01e012100fa603
VincentMarkk <vincent.markwijaya@gmail.com>
2023-06-14 07:58:11 +0700

    pimpinan page

--- public/scriptPimpinan.js
+++ public/scriptPimpinan.js
@@ -12,3 +12,11 @@ const adminSidebarSongs = document.getElementById('Admin-sidebar-songs');
 adminSidebarSongs.addEventListener('click', () => {
   window.location.href = '/songsAdmin'; 
 });
+
+
+function getCurrentDate() {
+    var currentDate = new Date();
+    var dateString = currentDate.toDateString();
+    var dateElement = document.getElementById("currentDate");
+    dateElement.textContent = dateString;
+}
