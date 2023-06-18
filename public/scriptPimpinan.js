94caf88dacad0d60025aa72371606fe0d8fd500f
marcelinoputra <marcelinoputra0701@gmail.com>
2023-06-18 22:00:01 +0700

    data grafik udh bener

--- public/scriptPimpinan.js
+++ public/scriptPimpinan.js
@@ -33,53 +33,91 @@ function getCurrentDate() {
   dateElement.textContent = dateString;
 }
 
-//diagram batang sales
-const ctb = document.getElementById('salesBarChart').getContext('2d');
-const barChart = new Chart(ctb, {
-type: 'bar',
-data: {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-  datasets: [{
-    label: 'Data',
-    data: [12, 19, 3, 5, 2, 3, 6, 10, 8, 7, 4, 15], //data sementara, blm connect db
-    backgroundColor: 'rgba(0, 123, 255, 0.5)', // Optional
-    borderColor: 'rgba(0, 123, 255, 1)', // Optional
-    borderWidth: 1 // Optional
-  }]
-},
-options: {
-  responsive: true,
-  scales: {
-    y: {
-      beginAtZero: true
-    }
-  }
-}
-});
+// Menggunakan Fetch API untuk mengambil data pembayaran dari server
+fetch('/dataPembayaran')
+  .then((response) => response.json())
+  .then((data) => {
+    // Data pembayaran diterima dari server, lanjutkan dengan membuat grafik
+    const bulan = data.map((item) => item.bulan);
+    const jumlahPembayaran = data.map((item) => item.jumlah_pembayaran);
 
-//diagram garis sales
-const ctl = document.getElementById('salesLineChart').getContext('2d');
-const lineChart = new Chart(ctl, {
-type: 'line',
-data: {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-  datasets: [{
-    label: 'Data',
-    data: [12, 19, 3, 5, 2, 3, 6, 10, 8, 7, 4, 15], //data sementara, blm connect db
-    backgroundColor: 'rgba(0, 123, 255, 0.5)', // Optional
-    borderColor: 'rgba(0, 123, 255, 1)', // Optional
-    borderWidth: 1 // Optional
-  }]
-},
-options: {
-  responsive: true,
-  scales: {
-    y: {
-      beginAtZero: true
-    }
-  }
-}
-});
+    // Menggunakan Chart.js untuk membuat grafik
+    const ctx = document.getElementById('salesBarChart').getContext('2d');
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: bulan,
+        datasets: [
+          {
+            label: 'Jumlah Pembayaran',
+            data: jumlahPembayaran,
+            backgroundColor: 'rgba(54, 162, 235, 0.5)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+    const ctx2 = document.getElementById('salesLineChart').getContext('2d');
+    new Chart(ctx2, {
+      type: 'line',
+      data: {
+        labels: bulan,
+        datasets: [
+          {
+            label: 'Jumlah Pembayaran',
+            data: jumlahPembayaran,
+            backgroundColor: 'rgba(54, 162, 235, 0.5)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  })
+  .catch((error) => {
+    console.error('Error fetching data:', error);
+  });
+
+
+// //diagram garis sales
+// const ctl = document.getElementById('salesLineChart').getContext('2d');
+// const lineChart = new Chart(ctl, {
+// type: 'line',
+// data: {
+//   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+//   datasets: [{
+//     label: 'Data',
+//     data: [12, 19, 3, 5, 2, 3, 6, 10, 8, 7, 4, 15], //data sementara, blm connect db
+//     backgroundColor: 'rgba(0, 123, 255, 0.5)', // Optional
+//     borderColor: 'rgba(0, 123, 255, 1)', // Optional
+//     borderWidth: 1 // Optional
+//   }]
+// },
+// options: {
+//   responsive: true,
+//   scales: {
+//     y: {
+//       beginAtZero: true
+//     }
+//   }
+// }
+// });
 
 function displaySearchAdminResults(results) {
 const tableBody = document.querySelector('.pimpinan-notmain-content-right tbody');
